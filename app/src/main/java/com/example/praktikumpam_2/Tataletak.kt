9ac892c24b5fcc4e2f4d28fd6bcd6dd4c464f35c
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-16 16:37:42 +0700

    Composable Jetpack Compose

--- app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
@@ -79,3 +79,15 @@ fun TataLetakRowColumn(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataLetakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.notasibalok)
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        ) {
