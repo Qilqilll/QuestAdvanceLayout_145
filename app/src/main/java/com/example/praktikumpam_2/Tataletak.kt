5e68cb28f2018fd5f4d3c30b57b8414c75a6b2f3
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-16 16:18:27 +0700

    Tata letak column

--- app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
@@ -1,4 +1,17 @@
 package com.example.praktikumpam_2
 
-import androidx.compose.runtime.composable
-import androidx
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start =20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+
+    }
+}
+
