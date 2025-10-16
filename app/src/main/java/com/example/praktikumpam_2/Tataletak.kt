0573056ab047318392fd8f45702c749daea07a6c
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-16 16:39:13 +0700

    baris teks sejajar horizontal

--- app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
@@ -91,3 +91,12 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
                 .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
         ) {
+            Column() {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
