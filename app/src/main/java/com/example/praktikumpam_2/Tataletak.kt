35a4f14610b12c856a8e57cebeef215db46d4663
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-16 17:09:45 +0700

    import dan rename

--- app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam_2/Tataletak.kt
@@ -1,9 +1,27 @@
 package com.example.praktikumpam_2
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakColumn(modifier: Modifier) {
@@ -66,13 +84,13 @@ fun TataLetakColumnRow(modifier: Modifier) {
 fun TataLetakRowColumn(modifier: Modifier) {
     Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
         //Kolom1
-        Column() {
+        Column(){
             Text(text = "Komponen1Kolom1")
             Text(text = "Komponen2Kolom1")
             Text(text = "Komponen3Kolom1")
         }
         //Kolom2
-        Column() {
+        Column(){
             (text = "Komponen1Kolom2")
             (text = "Komponen2Kolom2")
             (text = "Komponen3Kolom2")
