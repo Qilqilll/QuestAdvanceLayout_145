509bf10816323859b7414c91e41e39442c832bfd
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-10-23 16:05:21 +0700

    modifier

--- Praktikum3/app/src/main/java/UI_145.kt
+++ Praktikum3/app/src/main/java/UI_145.kt
@@ -73,4 +73,9 @@ fun ActivitasPertama(modifier: Modifier) {
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
                         color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
+        }
 
